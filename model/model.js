7c441f10cc4d0f6a6cfc05000e780268ae676376
simplysabir <simplysabir@gmail.com>
2024-02-07 20:39:39 +0530

    mongoose setup

---
+++ model/model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const serverSchema = new mongoose.Schema({
+  serverId: String,
+  creatorAddress: String,
+  eligibilityCriteria: {
+    common: Number,
+    legendary: Number,
+    rare: Number,
+    minimumNFTs: Number,
+  },
+  users: [
+    {
+      discordId: String,
+      walletAddress: String,
+      isEligible: Boolean,
+    },
+  ],
+});
+
+const Server = mongoose.model('Server', serverSchema);
+
+module.exports = { Server };
