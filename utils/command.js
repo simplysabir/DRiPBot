1a47c20c51e3255279cd24216b620aabed17af77
simplysabir <simplysabir@gmail.com>
2024-02-24 08:41:33 +0530

    command asycn updates

--- utils/command.js
+++ utils/command.js
@@ -47,12 +47,14 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-try {
-  console.log('Started refreshing application (/) commands.');
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
 
-  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands });
 
-  console.log('Successfully reloaded application (/) commands.');
-} catch (error) {
-  console.error(error);
-}
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
